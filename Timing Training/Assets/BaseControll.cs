aa2ba61cc7819534856a8e5f925a3a7e55c8a6a1
AIE\s181563 <ignislupus.wolfie@gmail.com>
2018-09-05 13:54:04 +0930

    Alpha v1

---
+++ Timing Training/Assets/BaseControll.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseControll : MonoBehaviour {
+
+    public float time = 0.0f;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Clock();
+        PressedSpace();
+
+	}
+
+    void Clock()
+    {
+
+        if(time != 8.0f)
+        {
+
+
+
+        }
+
+    }
+
+    void PressedSpace()
+    {
+
+        if(Input.GetButton("Space"))
+        {
+
+            Debug.Log("Space Pressed");
+
+        }
+
+    }
+}
