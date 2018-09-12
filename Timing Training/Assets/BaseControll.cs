fd208fce7cb2a9cc54285a327c3bf151980acb81
AIE\s181563 <ignislupus.wolfie@gmail.com>
2018-09-12 14:05:31 +0930

    no message

--- Timing Training/Assets/BaseControll.cs
+++ Timing Training/Assets/BaseControll.cs
@@ -5,43 +5,109 @@ using UnityEngine;
 public class BaseControll : MonoBehaviour {
 
     public float time = 0.0f;
+    public float shotTimer = 0.0f;
 
+    public bool opponentIsDead = true;
+    public bool triggerLock = false;
 
+    public GameObject Plain;
+    public GameObject Green;
+    public GameObject Yellow;
+    public GameObject Red;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update () {
 
         Clock();
         PressedSpace();
 
-	}
+        if(time >= 8.5)
+        {
 
+
+
+        }
+
+        if (time <= 9.0f)
+        {
+
+            time += Time.deltaTime;
+
+        }
+
+        if (time >= 8.5f)
+        {
+
+
+
+            Plain.SetActive(false);
+            Green.SetActive(false);
+            Yellow.SetActive(false);
+            Red.SetActive(true);
+
+        }
+
+    }
+
+    //tracking all the time based actions happens here
     void Clock()
     {
 
-        if(time != 8.0f)
+        if(opponentIsDead == true)
         {
 
+            if (time >= 8.0f)
+            {
 
+                opponentIsDead = false;
+
+            }
 
         }
 
+        if(triggerLock == true && shotTimer <= 0.5f)
+        {
+
+            shotTimer += Time.deltaTime;
+
+        }
+        else
+        {
+
+            triggerLock = false;
+            shotTimer = 0.0f;
+
+        }
+        
+
     }
 
+    //trigger script
     void PressedSpace()
     {
 
-        if(Input.GetButton("Space"))
+        if(Input.GetButtonDown("Space") && triggerLock == false)
         {
+            
+            triggerLock = true;
+            if(opponentIsDead == false && time <= 8.5f)
+            {
 
-            Debug.Log("Space Pressed");
+                opponentIsDead = true;
+                time = 0.0f;
+
+                Plain.SetActive(false);
+                Green.SetActive(true);
+                Yellow.SetActive(false);
+                Red.SetActive(false);
+
+            }
 
         }
 
+       
+
     }
+
+    
 }
